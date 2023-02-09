eca84c29e7a204d18fe3d088e8c140bd15122672
Jennifer Kim <jkjkim0217@gmail.com>
2023-02-09 11:49:49 -0800

    updated madlib 2/9

--- studio1/script.js
+++ studio1/script.js
@@ -8,23 +8,35 @@ const madLibs = document.querySelector('#madLib');
 
     myForm.addEventListener('submit', function(event){
         event.preventDefault();
+        document.querySelector('#output').className='show';
+        document.querySelector('#input').className='hide';
+
         const name = document.querySelector('#name').value;
         const place = document.querySelector('#place').value;
         const action = document.querySelector('#action').value;
-        const object = document.querySelector('#noun').value;
-        const plural = document.querySelector('#noun').value;
-        const living = document.querySelector('#noun').value;
+        const object = document.querySelector('#object').value;
+        const plural = document.querySelector('#plural').value;
+        const living = document.querySelector('#living').value;
 
         let myText;
         if(name && place && action && object && plural && living) {
-            myText = `Hello ${name}, this is a note to help you escape this ${place}! Before I help you, you have to ${action} for 400 hours or else this note will be useless. Once you have done that, you will walk out this door, and you will see a jumping ${object}. Once you pass that area, you will see glowing ${plural} and you should smile at them to look friendly. If you don't smile, they will run after you. After that, you will arrive at the end and you will know that you have reached the end when you see me. <br>Good luck!
+            myText = `Hello <b>${name}</b>,<br><br> This is a note to help you escape this miserable <b>${place}</b>! Before I help you, you have to <b>${action}</b> for 400 hours or else this note will be useless. Once you have done that, you will walk out this door and you will see a jumping <b>${object}</b>. Once you pass that thing, you will see glowing <b>${plural}</b> and smile at them to look friendly. If you don't smile, they will run after you. After that, you will see me at the end with a stopwatch. Good luck!
             <br>
             <br>
-            From ${living}`;
+            From <b>${living}</b>`;
+
+            // resetting form
+            document.querySelector('#name').value='';
+            document.querySelector('#place').value='';
+            document.querySelector('#action').value='';
+            document.querySelector('#object').value='';
+            document.querySelector('#plural').value='';
+            document.querySelector('#living').value='';
         } else {
-            myText = 'Please fill out all the blanks!'
+            myText = 'Please fill out all the blanks!';
         }
         madLibs.innerHTML = myText;
+
     });
 
 
@@ -35,15 +47,10 @@ const madLibs = document.querySelector('#madLib');
         document.querySelector('#intro').className='hide';
     });
 
-    document.querySelector('#submitButton').addEventListener('click', function(event){
-        event.preventDefault();
-        document.querySelector('#output').className='show';
-        document.querySelector('#input').className='hide';
-    });
-
     document.querySelector('#again').addEventListener('click', function(event){
         event.preventDefault();
-        document.querySelector('#input').className='show';
+        document.querySelector('#intro').className='show';
         document.querySelector('#output').className='hide';
+        
     });
-}())
+}());
