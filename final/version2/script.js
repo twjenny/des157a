e4ea532be3a11f44858e30233d75ff8876f167f4
Jennifer Kim <jkjkim0217@gmail.com>
2023-03-12 00:18:50 -0800

    version 2 added

---
+++ final/version2/script.js
@@ -0,0 +1,23 @@
+//loads images first before beginning the page
+('load', function () {
+	"use strict";
+	console.log("reading js");
+
+    const content = document.getElementById('sister');
+    const theImg = document.getElementsByClassName('sisterLetter');
+    const percent = content.offsetWidth / 100;
+    let prevLoc = 0;
+    
+    content.addEventListener('mousemove',reportPos);
+    function reportPos(event) {
+        const mousePosX = Math.ceil((event.clientX - (content.getBoundingClientRect().left))/percent);
+
+        if (prevLoc !== mousePosX) {
+            let addedPx = mousePosX * 30;
+            theImg.style.width = (800 + addedPx) + 'px';
+            prevLoc = mousePosX;
+            console.log(prevLoc);
+        }
+    }
+
+}());
