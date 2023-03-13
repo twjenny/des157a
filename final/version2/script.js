8e93affd272c68dc638fb26055937392c8dd00b4
Jennifer Kim <jkjkim0217@gmail.com>
2023-03-12 23:13:18 -0700

    version 2 user test script & update portal page complete

--- final/version2/script.js
+++ final/version2/script.js
@@ -20,4 +20,20 @@
         }
     }
 
+// user test alert message
+    alert(`
+    Welcome to my Final Project iteration and thank you for your time to take this user test. 
+    This test will take approximately 10 minutes and you will complete the three following goals:
+
+    1. Try to move to next section without scrolling.
+    2. When you reach the end of the webiste, move back to the top page with just one click.
+    3. Try to figure out the meaning of the project.
+
+    Feel free to say any thoughts that come to your mind, and just remember that there is no right or wrong answers here. Anything you say will be a great feedback for my final project!
+    
+    If you forget the goals, go ahead and refresh the page by pressing refresh button or shift+command+R to reload the page to see the alert.
+    
+    Once again, thank you for your participation!`);
+
+
 }());
